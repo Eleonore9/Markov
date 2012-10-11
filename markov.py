a2ca7e008ff691a9d8e07730857d276ebdb16e40
mercul3s <mercul3s@gmail.com>
2012-10-10 22:37:16 -0700

    fooled around with key / value generation

--- markov.py
+++ markov.py
@@ -2,58 +2,35 @@ from sys import argv
 import random
 
 file = argv[1]
-# making a comment to test new branch
+
 # open file for reading and assign file contents to variable filetext, return filetext
 def open_file(filename):
 	text = open(filename)
 	filetext = text.read()
 	text.close()
-	#print filetext
 	return filetext
 
 # split string filetext into a list, return the list
 def split_words(text):
 	text = text.split()
-	# print text
 	return text
 
-# create bigram dictionary from words in list
+# create bigram dictionary from words in list, return bigram dictionary
 def get_bigram(text):
 	bigram = {}
 	count = 0
+	# loop through the text and make key pairs, assign values
 	while count <= len(text) -3:
 		# 
 		key_list = (text[count], text[count + 1])
 		bigram[key_list] = text[count+2]
-		#bigram[[text[count]+ " " + text[count+1]]] = text[count+2]
-		#print bigram
 		count += 1
-		#print "at the bottom count is %d" % count
-	#print bigram
 	return bigram
-	# count = 0
-	# while count < len(text):
-	# 	text = text.replace(" ", "*")
-	# 	print text
-	# 	count += 2
-	# return text
 
 # need to define end of sentence as a period
 # need to check for duplicate keys and create multiple values for single keys
-
-
 # function to generate sentences from our dictionary
 # gets list of keys, passes that list to generator
-def generate_text(bigram_dict):
-	bigram_keys = bigram_dict.keys()
-	#print bigram_keys
-	# print a random bigram and value for each key in bigram dictionary1
-		#print random_bigram
-	random_bigram = random.choice(bigram_keys)
-	# search for second value of key + value as the next key to add to your sentence
-	second_bigram = random_bigram[1] , bigram_dict[random_bigram]
-
-
 ''' 
 for random value, get the key. To get the next key, take the 2nd element of the key
 and the value of that key. Use that key pair to lookup the next value. Rough idea:
@@ -66,25 +43,47 @@ and the value of that key. Use that key pair to lookup the next value. Rough ide
 		(c, d) = (e)
 
 A while loop may be more suitable for this operation. Also needed: how do we stop the loop?
-'''
-	for keys in bigram_dict.items():	
-		start_sentence = random_bigram
-		next_word = bigram_dict[random_bigram]
-		print start_sentence, next_word
-		print second_bigram
-		#if bigram_dict[second] != None:
-		if second_bigram in bigram_dict:
-			print bigram_dict[second_bigram]
-		else:
-			print "not found"
+'''	
+
+def generate_text(bigram_dict, text):
+	# get list of keys
+	bigram_keys = bigram_dict.keys()
+	# print a random bigram and value for each key in bigram dictionary
+	random_bigram = random.choice(bigram_keys)
+	# set first sentence based on bigram key and value
+	first_sentence = random_bigram, bigram_dict[random_bigram]
+	# search for second value of key + value as the next key to add to your sentence
+	second_bigram = random_bigram[1], bigram_dict[random_bigram]
+	second_sentence = bigram_dict[second_bigram]
+	#third_bigram = bigram_dict[bigram_dict[random_bigram]], bigram_dict[bigram_dict[second_bigram]]
+	# loop control variable
+	count = 0
+	while count < 1:
+		print first_sentence
+		print second_sentence
+		#new_text += random_bigram[0] + " " + random_bigram[1] + " " + bigram_dict[random_bigram[0]] + " " + bigram_dict[random_bigram[1]] + " " + bigram_dict[second_bigram]
+		#print new_text
+		count += 1
+
+	# for keys in bigram_dict.items():	
+	# 	start_sentence = random_bigram
+	# 	next_word = bigram_dict[random_bigram]
+	# 	print start_sentence, next_word
+	# 	print second_bigram
+	# 	#if bigram_dict[second] != None:
+	# 	if second_bigram in bigram_dict:
+	# 		print bigram_dict[second_bigram]
+	# 	else:
+	# 		print "not found"
 		
-		second_bigram = second_bigram[1], bigram_dict[second_bigram]
+		#second_bigram = second_bigram[1], bigram_dict[second_bigram]
 
 def main():
 	words = open_file(file)
 	words = split_words(words)
 	bigram_dictionary = get_bigram(words)
-	generate_text(bigram_dictionary)
+	print bigram_dictionary
+	generate_text(bigram_dictionary, words)
 
 main()
 
